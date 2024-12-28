4e35885354ac06d345ba74f0457e980aac992d61
Antony-Stanlin <antonystanlin0@gmail.com>
2024-12-28 18:54:31 +0530

    Add files via upload

---
+++ taskmanager/com/taskmaster/TaskMaster.java
@@ -0,0 +1,17 @@
+package com.taskmanager;
+
+import com.taskmanager.setup.TaskView;
+
+public class TaskMaster {
+
+    public static void main(String ar[]){
+
+        new TaskView().init();
+
+        System.out.println("Thank you for using Task Manager,Visit again");
+
+
+    }
+}
+
+
